4da647a3f8fea1828ced32806118105e47bb3583
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2024-05-30 21:24:36 +0200

    Update BentoGrid description font size and styling

--- src/data/index.ts
+++ src/data/index.ts
@@ -49,7 +49,7 @@ export const navItems = [
     {
       id: 5,
       title: "Currently building a Next.js Eccomerce site.",
-      description: "The Inside Scoop",
+      description: "Behind the scenes",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
       titleClassName: "justify-center md:justify-start lg:justify-center",
