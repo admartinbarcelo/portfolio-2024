e2c92115818d213000d57724a4141a498188ce5f
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2024-06-11 12:02:00 +0200

    refactor: Update Projects section with new images and assets

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -12,12 +12,12 @@ export const Projects = () => {
         <span className="text-purple">recent projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
-        {projectItems.map((item) => (
+        {projectItems.map(({id, title, description, img, iconLists, link, hrefRepository}) => (
           <div
             className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
-            key={item.id}
+            key={id}
           >
-            <PinContainer title={item.link} href={item.link}>
+            <PinContainer title={link} href={link}>
               <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
@@ -31,31 +31,31 @@ export const Projects = () => {
                   />
                 </div>
                 <Image
-                  src={item.img}
+                  src={img}
                   alt="cover"
                   layout="fill"
                   objectFit="cover"
-                  className="z-10 absolute bottom-0 w-full h-full overflow-hidden lg:rounded-3xl "
+                  className="z-10 absolute bottom-0 "
                 />
               </div>
 
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                {item.title}
+                {title}
               </h1>
 
               <p
-                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-3"
+                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
                 style={{
                   color: "#BEC1DD",
                   margin: "1vh 0",
                 }}
               >
-                {item.description}
+                {description}
               </p>
 
               <div className="flex items-center justify-between mt-7 mb-3">
                 <div className="flex items-center">
-                  {item.iconLists.map((icon, index) => (
+                  {iconLists.map((icon, index) => (
                     <div
                       key={index}
                       className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
@@ -77,7 +77,7 @@ export const Projects = () => {
                 <div className="flex justify-center items-center">
                   <Link
                     target="_blank"
-                    href={item.hrefRepository ?? ""}
+                    href={hrefRepository ?? ""}
                     className="flex lg:text-xl md:text-xs text-sm text-purple"
                   >
                     Check The Respository
